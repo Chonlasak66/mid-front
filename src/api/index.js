c5dbf9b566a8928d4216fe56e79d58cf43a0a30e
chonlasak <chonlasak@rmutl.ac.th>
2024-03-06 14:02:28 +0700

    url

--- src/api/index.js
+++ src/api/index.js
@@ -18,7 +18,7 @@ export async function login({ email, password }) {
 }
 
 export async function searchArtworks({ keyword }) {
-  return await fetch(`/api/homepage/getArtworks/${keyword}`, {
+  return await fetch(`http://api-user66008.se-rmutl.net/api/homepage/getArtworks/${keyword}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   })
